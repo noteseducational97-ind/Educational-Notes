5361357a2ac114e454741023d772e1f50a1f9180
Educational Notes <noteseducational97@gmail.com>
2025-08-21 09:30:36 +0000

    Make the following changes:
    ,
    
    Find this element:
    
      <Primitive.li role

--- src/lib/firebase/server.ts
+++ src/lib/firebase/server.ts
@@ -9,7 +9,12 @@ if (!serviceAccountString) {
   throw new Error('FIREBASE_SERVICE_ACCOUNT_KEY environment variable is not set.');
 }
 
-const serviceAccount = JSON.parse(serviceAccountString);
+let serviceAccount;
+try {
+  serviceAccount = JSON.parse(serviceAccountString);
+} catch (e) {
+    throw new Error('Failed to parse FIREBASE_SERVICE_ACCOUNT_KEY. Make sure it is a valid JSON string.');
+}
 
 // Manually replace escaped newlines in the private key
 if (serviceAccount.private_key) {
