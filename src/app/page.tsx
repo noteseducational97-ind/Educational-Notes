523a856839d7d2387d8a241908b64e3bdc626efa
Educational Notes <noteseducational97@gmail.com>
2025-08-21 11:14:30 +0000

    Welcome To Educational Notes Your Mission To Archieve Best Educational

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -63,7 +63,7 @@ export default function Home() {
                   <h1 className="text-3xl font-bold tracking-tighter sm:text-5xl xl:text-6xl/none text-primary">
                     Welcome To Educational Notes
                   </h1>
-                   <p className="max-w-[600px] text-muted-foreground md:text-xl">High Quality Notes with all things related to study but free of cost</p>
+                   <p className="max-w-[600px] text-muted-foreground md:text-xl">Your Mission To Achieve Best Educational Material with High Quality Notes with all things related to study but free of cost</p>
                 </div>
               </div>
               <Image
