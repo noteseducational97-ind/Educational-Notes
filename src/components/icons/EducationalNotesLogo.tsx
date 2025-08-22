8fb3c222fd11d139d4fcd1e441faa8fae7573a21
Educational Notes <noteseducational97@gmail.com>
2025-08-22 07:38:33 +0000

    Make the following changes:
    ,
    
    Find this element:
    
      <EducationalNotesL

--- src/components/icons/EducationalNotesLogo.tsx
+++ src/components/icons/EducationalNotesLogo.tsx
@@ -15,12 +15,8 @@ export function EducationalNotesLogo(props: SVGProps<SVGSVGElement>) {
       strokeLinejoin="round"
       {...props}
     >
-      <rect width="8" height="4" x="8" y="2" rx="1" ry="1"/>
-      <path d="M16 4h2a2 2 0 0 1 2 2v14a2 2 0 0 1-2 2H6a2 2 0 0 1-2-2V6a2 2 0 0 1 2-2h2"/>
-      <path d="M12 11h4"/>
-      <path d="M12 16h4"/>
-      <path d="M8 11h.01"/>
-      <path d="M8 16h.01"/>
+      <path d="M2 3h6a4 4 0 0 1 4 4v14a3 3 0 0 0-3-3H2z"/>
+      <path d="M22 3h-6a4 4 0 0 0-4 4v14a3 3 0 0 1 3-3h7z"/>
     </svg>
   );
 }
