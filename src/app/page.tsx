4d7812b800d842ac5a13c041d894f48c10653102
Educational Notes <noteseducational97@gmail.com>
2025-08-22 10:28:37 +0000

    Make the following changes:
    ,
    
    Find this element:
    
    Relevant files:

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -75,11 +75,11 @@ export default function Home() {
                 </div>
               </div>
               <Image
-                src="https://images.pexels.com/photos/8617965/pexels-photo-8617965.jpeg"
+                src="https://images.pexels.com/photos/159711/books-bookstore-book-reading-159711.jpeg"
                 width="600"
                 height="400"
-                alt="A group of Indian college students studying together."
-                data-ai-hint="group study"
+                alt="Students studying in a library."
+                data-ai-hint="library study"
                 className="mx-auto aspect-video overflow-hidden rounded-xl object-cover sm:w-full lg:order-last lg:aspect-square shadow-2xl"
               />
             </div>
