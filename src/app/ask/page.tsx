523f7d4e2629cbbe1a56a5545d78ee99bb992e83
Educational Notes <noteseducational97@gmail.com>
2025-08-23 11:23:34 +0000

    Make the following changes:
    ,
    
    Find this element:
    
      <AskForm>
    
    with th

--- src/app/ask/page.tsx
+++ src/app/ask/page.tsx
@@ -1,3 +1,4 @@
+'use client';
 
 import Header from '@/components/layout/Header';
 import AskForm from './_components/AskForm';
@@ -6,8 +7,10 @@ export default function AskPage() {
   return (
     <div className="flex h-screen flex-col bg-background">
       <Header />
-      <main className="flex-1 flex items-center justify-center p-4">
-        <AskForm />
+      <main className="flex-1 flex items-center justify-center p-4 overflow-hidden">
+        <div className="h-full w-full flex items-center justify-center">
+            <AskForm />
+        </div>
       </main>
     </div>
   );
