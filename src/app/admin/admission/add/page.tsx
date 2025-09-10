0520d10a9b44ad9ff7be3185ccc6d12350afc6b7
Educational Notes <noteseducational97@gmail.com>
2025-09-10 08:40:49 +0000

    move below (_for element <FormItem>_)

--- src/app/admin/admission/add/page.tsx
+++ src/app/admin/admission/add/page.tsx
@@ -215,6 +215,22 @@ export default function AddAdmissionFormPage() {
                             </FormItem>
                         )} />
                         <div className="grid md:grid-cols-2 gap-4">
+                             <FormField control={form.control} name="subject" render={({ field }) => (
+                                <FormItem>
+                                    <FormLabel className="flex items-center gap-2"><Book /> Subject</FormLabel>
+                                    <FormControl><Input {...field} value={field.value || ''} /></FormControl>
+                                    <FormMessage />
+                                </FormItem>
+                            )} />
+                            <FormField control={form.control} name="className" render={({ field }) => (
+                                <FormItem>
+                                    <FormLabel>Class Name</FormLabel>
+                                    <FormControl><Input placeholder="e.g. Class 11 / MHT-CET" {...field} value={field.value || ''} /></FormControl>
+                                    <FormMessage />
+                                </FormItem>
+                            )} />
+                        </div>
+                        <div className="grid md:grid-cols-2 gap-4">
                             <FormField control={form.control} name="title" render={({ field }) => (
                                 <FormItem>
                                     <FormLabel>Batch Title</FormLabel>
@@ -233,22 +249,6 @@ export default function AddAdmissionFormPage() {
                                             ))}
                                         </SelectContent>
                                     </Select>
-                                    <FormMessage />
-                                </FormItem>
-                            )} />
-                        </div>
-                        <div className="grid md:grid-cols-2 gap-4">
-                            <FormField control={form.control} name="subject" render={({ field }) => (
-                                <FormItem>
-                                    <FormLabel className="flex items-center gap-2"><Book /> Subject</FormLabel>
-                                    <FormControl><Input {...field} value={field.value || ''} /></FormControl>
-                                    <FormMessage />
-                                </FormItem>
-                            )} />
-                            <FormField control={form.control} name="className" render={({ field }) => (
-                                <FormItem>
-                                    <FormLabel>Class Name</FormLabel>
-                                    <FormControl><Input placeholder="e.g. Class 11 / MHT-CET" {...field} value={field.value || ''} /></FormControl>
                                     <FormMessage />
                                 </FormItem>
                             )} />
